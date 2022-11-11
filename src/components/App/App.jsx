192b39c32f7077a7ff0353ab416ab3322d86c1ed
Alexander Shabanov <99245672+netmarketolog@users.noreply.github.com>
2022-11-11 17:55:17 +0200

    fixed

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,13 @@
 import { Component } from 'react';
-import { Searchbar } from './Searchbar/Searchbar';
-import fetchValue from '../Api/Api';
-import ImageGallery from './ImageGallery/ImageGallery';
-import { Loader } from './Loader/Loader';
-import Button from './Button/Button';
-import { Container } from './App.styled';
+
+import { Searchbar } from '../Searchbar/Searchbar';
+import ImageGallery from '../ImageGallery/ImageGallery';
+import { Loader } from '../Loader/Loader';
+import Button from '../Button/Button';
+import fetchValue from '../../api/api';
+
+import { Container } from '../App/App.styled';
+
 export class App extends Component {
   state = {
     inputValue: '',
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,5 +1,5 @@
-import { Gallery } from './ImageGallery.styled';
 import { ImageGalleryItem } from './ImageGalleryItem/ImageGalleryItem';
+import { Gallery } from './ImageGallery.styled';
 import PropTypes from 'prop-types';
 
 export default function ImageGallery({ data }) {
