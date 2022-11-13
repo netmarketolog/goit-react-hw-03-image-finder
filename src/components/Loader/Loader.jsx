0fc4d726b1a981afb36eb7012a4b2be0c8d1b77f
Alexander Shabanov <99245672+netmarketolog@users.noreply.github.com>
2022-11-13 20:52:50 +0200

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -2,7 +2,7 @@ import { InfinitySpin } from 'react-loader-spinner';
 
 export const Loader = () => {
   return (
-    <div>
+    <div style={{ margin: '20px auto' }}>
       <InfinitySpin width="200" color="#4fa94d" />
     </div>
   );
